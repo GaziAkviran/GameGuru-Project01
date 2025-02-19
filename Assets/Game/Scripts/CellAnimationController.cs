1b897f3b3c08a87be24fc289d92f2e0d5b2940d3
Gazi Akviran <gaziakvrn@gmail.com>
2025-02-19 17:55:38 +0300

    Dotween package added
    - Dotween added for tweening
    - Select and deselect animation implemented
    - Cell celet and deselect color changes added

---
+++ Assets/Game/Scripts/CellAnimationController.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class CellAnimationController : MonoBehaviour
+{
+    [SerializeField, BoxGroup("Text Animation Setup")] private float textAppearDuration = 0.2f;
+    [SerializeField, BoxGroup("Text Animation Setup")] private float textDisappearDuration = 0.1f;
+
+    [SerializeField, BoxGroup("Cell Animation Setup")] private float colorChangeDuration = 0.2f;
+    [SerializeField, BoxGroup("Cell Animation Setup")] private Color cellDeselectColor;
+    [SerializeField, BoxGroup("Cell Animation Setup")] private Color cellSelectColor;
+
+    [SerializeField, BoxGroup("Cell Popup Animation Setup")] private float selectScaleMultiplier = 1.1f;
+    [SerializeField, BoxGroup("Cell Popup Animation Setup")] private float selectScaleDuration = 0.15f;
+    
+    [SerializeField, BoxGroup("Cell Popup Animation Setup")] private float deselectScaleMultiplier = 0.9f;
+    [SerializeField, BoxGroup("Cell Popup Animation Setup")] private float deselectScaleDuration = 0.1f;
+    
+    [SerializeField, Foldout("References")] private Transform textTransform;
+    [SerializeField, Foldout("References")] private SpriteRenderer cellSpriteRenderer;
+    [SerializeField, Foldout("References")] Transform visualTransform;
+    
+    
+    private Tween textSelectTween, cellColorTween, cellSelectTween;
+
+    public void Init()
+    {
+        if (cellSpriteRenderer != null)
+        {
+            cellSpriteRenderer.material = new Material(cellSpriteRenderer.material);
+        }
+        
+        PlayCellDeselectAnimation(true);
+    }
+    
+    [Button()]
+    private void PlayTextShowAnimation(bool isInstant = false)
+    {
+        if (textTransform == null)
+        {
+            Debug.LogError("TextTransform is null. Please assign a transform");
+            return;
+        }
+
+        if (isInstant)
+        {
+            textTransform.localScale = Vector3.one;
+        }
+        else
+        {
+            textSelectTween?.Kill();
+
+            textSelectTween = textTransform.DOScale(Vector3.one, textAppearDuration)
+                .SetEase(Ease.OutBack);
+        }
+        
+    }
+
+    [Button()]
+    private void PlayTextHideAnimation(bool isInstant = false)
+    {
+        if (textTransform == null)
+        {
+            Debug.LogError("TextTransform is null. Please assign a Transform");
+            return;
+        }
+        
+        if (isInstant)
+        {
+            textTransform.localScale = Vector3.zero;
+        }
+        else
+        {
+            textSelectTween?.Kill();
+
+            textSelectTween = textTransform.DOScale(Vector3.zero, textDisappearDuration)
+                .SetEase(Ease.InBack);
+        }
+    }
+
+    [Button()]
+    public void PlayCellSelectAnimation(bool isInstant = false)
+    {
+        if (cellSpriteRenderer == null)
+        {
+            Debug.LogError("CellSpriteRenderer is null. Please assign a SpriteRenderer.");
+            return;
+        }
+
+        if (isInstant)
+        {
+            cellSpriteRenderer.color = cellSelectColor;
+        }
+        else
+        {
+            cellColorTween?.Kill();
+        
+            PlayCellSelectPopupAnimation();
+            PlayTextShowAnimation();
+            cellColorTween = cellSpriteRenderer.DOColor(cellSelectColor, colorChangeDuration)
+                .SetEase(Ease.OutQuad);
+        }
+        
+    }
+
+    [Button()]
+    public void PlayCellDeselectAnimation(bool isInstant = false)
+    {
+        if (cellSpriteRenderer == null)
+        {
+            Debug.LogError("CellSpriteRenderer is null. Please assign a SpriteRenderer.");
+            return;
+        }
+
+        if (isInstant)
+        {
+            cellSpriteRenderer.color = cellDeselectColor;
+        }
+        else
+        {
+            cellColorTween?.Kill();
+        
+            PlayCellDeselectPopupAnimation();
+            PlayTextHideAnimation();
+            cellColorTween = cellSpriteRenderer.DOColor(cellDeselectColor, colorChangeDuration)
+                .SetEase(Ease.OutQuad);
+        }
+    }
+
+    
+    private void PlayCellSelectPopupAnimation()
+    {
+        if (visualTransform == null)
+        {
+            Debug.LogError("Visual is null. Please assign a Transform");
+            return;
+        }
+        
+        cellSelectTween?.Kill();
+        
+        cellSelectTween = visualTransform.DOScale(Vector3.one * selectScaleMultiplier, selectScaleDuration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() => visualTransform.DOScale(Vector3.one, selectScaleDuration).SetEase(Ease.InQuad));
+    }
+
+    private void PlayCellDeselectPopupAnimation()
+    {
+        if (visualTransform == null)
+        {
+            Debug.LogError("Visual is null. Please assign a Transform");
+            return;
+        }
+        
+        cellSelectTween?.Kill();
+        
+        cellSelectTween = visualTransform.DOScale(Vector3.one * deselectScaleMultiplier, deselectScaleDuration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() => visualTransform.DOScale(Vector3.one, deselectScaleDuration).SetEase(Ease.InSine));
+        
+    }
+}
--- Assets/Game/Scripts/CellController.cs
+++ Assets/Game/Scripts/CellController.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 using Zenject;
 
 public class CellController : MonoBehaviour
 {
+    [SerializeField, Foldout("References")] CellAnimationController animationController;
     private Vector2Int position;
     private bool isMarked;
     private IGridView gridView;
@@ -20,11 +23,36 @@ public class CellController : MonoBehaviour
     {
         position = new Vector2Int(row, col);
         isMarked = false;
+        animationController.Init();
     }
 
     public void SetState(bool state)
     {
         isMarked = state;
+        UpdateVisual();
     }
-   
+
+    private void UpdateVisual()
+    {
+        Transform xMark = transform.Find("Visual/CellSprite");
+        if (xMark != null)
+        {
+            xMark.gameObject.SetActive(isMarked);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ToggleState();
+        }
+    }
+
+    private void ToggleState()
+    {
+        isMarked = !isMarked;
+        UpdateVisual();
+        gridView.UpdateCellVisual(position.x, position.y, isMarked);
+    }
 }
